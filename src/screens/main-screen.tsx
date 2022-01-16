74239a1d03a311d43ac208eb183b5fb2c3f24f8f
Ayushman Gupta <ayushman1998.1998@gmail.com>
2022-01-16 12:02:18 +0530

    created toggle theme switch

---
+++ src/screens/main-screen.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import {
+  Text,
+  Box,
+  Center,
+  VStack,
+  themeTools,
+  useTheme,
+  useColorMode,
+  useColorModeValue,
+} from 'native-base'
+import ThemeToggle from '../components/theme-toggle'
+export default function MainScreen() {
+  return (
+    <Center
+      _dark={{ bg: 'blueGray.900' }}
+      _light={{ bg: 'darkBlue.50' }}
+      height={'100%'}
+      px={4}
+      flex={1}
+      safeArea
+    >
+      <VStack space={5} alignItems={'center'}>
+        <Box>
+          <ThemeToggle />
+        </Box>
+      </VStack>
+    </Center>
+  )
+}
