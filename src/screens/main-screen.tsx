e747bd4d87a5a682529518076c2ece51cba2f4f1
Ayushman Gupta <ayushman1998.1998@gmail.com>
2022-01-16 12:57:44 +0530

    using bouncy checkbox package

--- src/screens/main-screen.tsx
+++ src/screens/main-screen.tsx
@@ -10,6 +10,8 @@ import {
   useColorModeValue,
 } from 'native-base'
 import ThemeToggle from '../components/theme-toggle'
+import BouncyCheckbox from "react-native-bouncy-checkbox";
+
 export default function MainScreen() {
   return (
     <Center
@@ -21,6 +23,9 @@ export default function MainScreen() {
       safeArea
     >
       <VStack space={5} alignItems={'center'}>
+        <Box w="100px" h="100px">
+        <BouncyCheckbox onPress={(isChecked: boolean) => {}} />
+        </Box>
         <Box>
           <ThemeToggle />
         </Box>
